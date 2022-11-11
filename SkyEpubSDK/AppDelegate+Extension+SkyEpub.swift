dfcd07adfcf70872321f90ab721c8a26062804b4
Amr Elghadban <amr.elghadban@gmail.com>
2022-11-11 12:38:12 +0400

    separate data

---
+++ SkyEpubSDK/AppDelegate+Extension+SkyEpub.swift
@@ -0,0 +1,16 @@
+//
+//  AppDelegate+Extension+SkyEpub.swift
+//  EpubBookReader
+//
+//  Created by Amr Elghadban on 11/11/2022.
+//
+
+import Foundation
+
+extension AppDelegate {
+    
+    func configureSkyEpubData() {
+        self.data = SkyData()
+    }
+    
+}
